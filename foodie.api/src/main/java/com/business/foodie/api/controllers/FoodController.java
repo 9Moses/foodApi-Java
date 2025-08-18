8d487f8496cfee560e4823fc42fbf26dc732147e
9Moses <esselmoses12@gmail.com>
2025-08-18 12:21:50 +0000

    controller/addfoods

---
+++ foodie.api/src/main/java/com/business/foodie/api/controllers/FoodController.java
@@ -0,0 +1,43 @@
+package com.business.foodie.api.controllers;
+
+import com.business.foodie.api.io.FoodRequest;
+import com.business.foodie.api.io.FoodResponse;
+import com.business.foodie.api.serivce.FoodService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/foods")
+@AllArgsConstructor
+public class FoodController {
+
+    @Autowired
+    private final FoodService foodService;
+
+    @PostMapping()
+    public FoodResponse addFood(@RequestPart("food") String foodString,
+                                @RequestPart("file")MultipartFile file){
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        FoodRequest request = null;
+
+        try{
+           request = objectMapper.readValue(foodString, FoodRequest.class);
+        }catch (JsonProcessingException ex){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Json format");
+        }
+
+        FoodResponse response = foodService.addFood(request, file);
+        return response;
+    }
+}
