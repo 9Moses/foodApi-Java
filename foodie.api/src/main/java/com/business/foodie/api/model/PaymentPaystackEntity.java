2327a9d26467e0bc22486675373338171ecb5f8b
9Moses <esselmoses12@gmail.com>
2025-08-28 16:36:56 +0000

    order with no payment api

---
+++ foodie.api/src/main/java/com/business/foodie/api/model/PaymentPaystackEntity.java
@@ -0,0 +1,49 @@
+package com.business.foodie.api.model;
+
+import com.business.foodie.api.enums.PricingPlanType;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Document(collection = "paystack_payment")
+public class PaymentPaystackEntity {
+    @Id
+    private Long id;
+
+    private String userId;
+
+    @Field(name = "reference")
+    private String reference;
+
+    @Field(name = "amount")
+    private BigDecimal amount;
+
+    @Field(name = "gateway_response")
+    private String gatewayResponse;
+
+    @Field(name = "paid_at")
+    private String paidAt;
+
+    @Field(name = "created_at")
+    private String createdAt;
+
+    @Field(name = "channel")
+    private String channel;
+
+    @Field(name = "currency")
+    private String currency;
+
+    @Field(name = "ip_address")
+    private String ipAddress;
+
+    @Field("pricing_plan_type")
+    private PricingPlanType pricingPlanType = PricingPlanType.BASIC;
+
+    @CreatedDate
+    @Field("created_on")
+    private Date createdOn;
+}
